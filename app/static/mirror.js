fd79090191121f19f45878a9c8dd8c60af875f73
Piotr <pksiazek.py@gmail.com>
2021-06-23 22:01:38 +0200

    bugfix heroku

--- app/static/mirror.js
+++ app/static/mirror.js
@@ -1,10 +1,13 @@
 const playlistSelect = document.querySelector(".playlists-select");
 console.log("hello there");
-axios.get("/get_user_playlists").then(function ({ data }) {
-  for (let playlist of data) {
-    const option = document.createElement("option");
-    option.value = playlist.id;
-    option.innerText = playlist.name;
-    playlistSelect.append(option);
-  }
-});
+axios
+  .get("/get_user_playlists")
+  .then(function ({ data }) {
+    for (let playlist of data) {
+      const option = document.createElement("option");
+      option.value = playlist.id;
+      option.innerText = playlist.name;
+      playlistSelect.append(option);
+    }
+  })
+  .catch((error) => console.log(error));
