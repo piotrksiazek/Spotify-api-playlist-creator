8d35e189bf06c6225359cbbf34264605efe5ceff
Piotr <pksiazek.py@gmail.com>
2021-06-23 22:51:41 +0200

    add additional app config

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ import config
 from config import Config
 from flask_login import LoginManager
 from .spotify_api_requests import spotify_api_requests
-
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.register_blueprint(spotify_api_requests, url_prefix="")
 
 from app import routes, models
