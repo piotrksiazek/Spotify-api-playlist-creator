f33a2ea648e7fc510c892c0b004affb32a9af3f5
Piotr <pksiazek.py@gmail.com>
2021-06-24 10:46:02 +0200

    session debugging

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from app import app
-if __name__ == '__main__':
-    print("XDDDDD")
-    app.run(debug=True)
-    app.debug = True
+# if __name__ == '__main__':
+#     print("XDDDDD")
+#     app.run(debug=True)
+app.debug = True
 # @app.shell_context_processor
 # def make_shell_context():
 #     return {'db': db, 'User': User}
